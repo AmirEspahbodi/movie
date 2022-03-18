842c36cdcd2ef7c2ce6411c9a9888f399257cd6c
amir hossein <amirhossein.espahbodi@gmail.com>
2022-03-18 19:37:12 +0330

    change dynamic routes, move dynamic part to the end

--- videos/urls.py
+++ videos/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', VideoListAPIView.as_view(), name='video_list'),
     path('upload/', VideoCreateAPIView.as_view(), name='video_upload'),
     path('<int:pk>/', VideoRetrieveAPIView.as_view(), name='video_detail'),
-    path('<int:pk>/update/', VideoUpdateAPIView.as_view(), name='video_update'),
-    path('<int:pk>/delete/', VideoDestroyAPIView.as_view(), name='video_destroy'),
+    path('update/<int:pk>/', VideoUpdateAPIView.as_view(), name='video_update'),
+    path('delete/<int:pk>/', VideoDestroyAPIView.as_view(), name='video_destroy'),
 ]
